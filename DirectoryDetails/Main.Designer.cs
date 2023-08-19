fa0f7b7e12c20ddc5e7678daccaa1b7337628108
Hassan Ansari <33068452+hassanansari92@users.noreply.github.com>
2023-08-20 01:29:06 +0500

    - Initial Commit

---
+++ DirectoryDetails/Main.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace DirectoryDetails
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tvControl = new System.Windows.Forms.TreeView();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fbd = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // tvControl
+            // 
+            this.tvControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tvControl.Location = new System.Drawing.Point(49, 96);
+            this.tvControl.Name = "tvControl";
+            this.tvControl.Size = new System.Drawing.Size(838, 616);
+            this.tvControl.TabIndex = 0;
+            this.tvControl.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvControl_AfterSelect);
+            this.tvControl.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.tvControl_NodeMouseDoubleClick);
+            this.tvControl.DoubleClick += new System.EventHandler(this.tvControl_DoubleClick);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(845, 42);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(44, 30);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "...";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(160, 46);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(675, 22);
+            this.txtPath.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select Directory";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 724);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.tvControl);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tvControl;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog fbd;
+    }
+}
+
---
+++ DirectoryDetails/Main.cs
@@ -0,0 +1,191 @@
+﻿using MediaInfoLib;
+using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DirectoryDetails
+{
+    public partial class Main : Form
+    {
+        //private string videoDirectory = @"C:\Hassan\Courses\Python\Complete Python Bootcamp HD";
+        private MediaInfo mediaInfo;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+
+        private async Task LoadVideoFilesAsync(string directoryPath)
+        {
+            tvControl.Nodes.Clear();
+            DirectoryInfo directoryInfo = new DirectoryInfo(directoryPath);
+
+            await TraverseDirectoriesAsync(directoryInfo, tvControl.Nodes);
+        }
+
+        private async Task TraverseDirectoriesAsync(DirectoryInfo directory, TreeNodeCollection parentNodes)
+        {
+            double fileSize = 0;
+            double duration = 0;
+
+            var directories = directory.GetDirectories();
+
+            foreach (var subDirectory in directories)
+            {
+                //fileSize = await Task.Run(() => subDirectory.EnumerateFiles("*", SearchOption.AllDirectories).Sum(file => file.Length) / (1024 * 1024));
+                fileSize = await Task.Run(() => CalculateDirectorySize(subDirectory.FullName));
+                fileSize = fileSize / (1024 * 1024);
+
+                //duration = await Task.Run(() => subDirectory.GetFiles("*.*").Where(file => file.Extension.ToLower() == ".mp4" || file.Extension.ToLower() == ".avi").Sum(f => GetVideoLengthAsync(f.FullName).Result.Seconds));
+                duration = await Task.Run(() => CalculateDirectoryDuration(subDirectory.FullName));
+
+                TreeNode directoryNode = new TreeNode(subDirectory.Name + " | " + TimeSpan.FromSeconds(duration).ToString(@"hh\:mm\:ss") + " | " + (fileSize > 1024 ? Math.Round((fileSize / 1024), 2) + " GB" : Math.Round(fileSize,2) + " MB"));
+
+                directoryNode.Tag = subDirectory.FullName;
+                parentNodes.Add(directoryNode);
+                await TraverseDirectoriesAsync(subDirectory, directoryNode.Nodes);
+            }
+
+            foreach (var fileInfo in directory.GetFiles("*.*").Where(file => file.Extension.ToLower() == ".mp4" || file.Extension.ToLower() == ".avi" || file.Extension.ToLower() == ".mkv"))
+            {
+                TimeSpan videoLength = await GetVideoLengthAsync(fileInfo.FullName);
+                double size = fileInfo.FullName.Length / (1024 * 1024);
+
+                TreeNode fileNode = new TreeNode(fileInfo.Name + " | " + videoLength.ToString(@"hh\:mm\:ss") + " | " + (size > 1024 ? Math.Round((size / 1024), 2) + " GB" : Math.Round(size,2) + " MB"));
+
+                //fileSize += fileInfo.FullName.Length;
+                //duration += videoLength.TotalSeconds;
+
+                fileNode.Tag = fileInfo.FullName;
+                parentNodes.Add(fileNode);
+            }
+        }
+
+        public double CalculateDirectoryDuration(string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                throw new DirectoryNotFoundException("Directory not found.");
+            }
+
+            double totalDuration = 0;
+
+            // Calculate size of files in the current directory
+            string[] files = Directory.GetFiles(directoryPath);
+
+            //Parallel.ForEach(files, file =>
+            //{
+            //    FileInfo fileInfo = new FileInfo(file);
+            //    totalDuration += GetVideoLengthAsync(fileInfo.FullName).Result.TotalSeconds;
+            //});
+
+            foreach (string file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                totalDuration += GetVideoLengthAsync(fileInfo.FullName).Result.TotalSeconds;
+            }
+
+            // Calculate size of subdirectories
+            string[] subDirectories = Directory.GetDirectories(directoryPath);
+            foreach (string subDirectory in subDirectories)
+            {
+                totalDuration += CalculateDirectoryDuration(subDirectory); // Recursively calculate nested subdirectory sizes
+            }
+
+            return totalDuration;
+        }
+
+        public long CalculateDirectorySize(string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                throw new DirectoryNotFoundException("Directory not found.");
+            }
+
+            long totalSize = 0;
+
+            // Calculate size of files in the current directory
+            string[] files = Directory.GetFiles(directoryPath);
+            foreach (string file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                totalSize += fileInfo.Length;
+            }
+
+            // Calculate size of subdirectories
+            string[] subDirectories = Directory.GetDirectories(directoryPath);
+            foreach (string subDirectory in subDirectories)
+            {
+                totalSize += CalculateDirectorySize(subDirectory); // Recursively calculate nested subdirectory sizes
+            }
+
+            return totalSize;
+        }
+        private async void tvControl_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node != null)
+            {
+                string filePath = e.Node.Tag as string;
+                if (!string.IsNullOrEmpty(filePath))
+                {
+                    TimeSpan videoLength = await GetVideoLengthAsync(filePath);
+
+                    //fileSizeLabel.Text = $"File Size: {new FileInfo(filePath).Length / (1024 * 1024)} MB";
+                    //videoLengthLabel.Text = $"Video Length: {videoLength.TotalHours:F2} hours";
+                }
+                else
+                {
+                    //fileSizeLabel.Text = "File Size: N/A";
+                    //videoLengthLabel.Text = "Video Length: N/A";
+                }
+            }
+        }
+
+        private async Task<TimeSpan> GetVideoLengthAsync(string filePath)
+        {
+            return await Task.Run(() =>
+            {
+                mediaInfo = new MediaInfo();
+                mediaInfo.Open(filePath);
+                string duration = mediaInfo.Get(StreamKind.General, 0, "Duration");
+                mediaInfo.Close();
+
+                if (double.TryParse(duration, out double durationMs))
+                {
+                    TimeSpan videoLength = TimeSpan.FromMilliseconds(durationMs);
+                    return videoLength;
+                }
+                return TimeSpan.Zero;
+            });
+        }
+
+        private void tvControl_DoubleClick(object sender, EventArgs e)
+        {
+        }
+
+        private void tvControl_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            string filePath = e.Node.Tag as string;
+            Process.Start(filePath);
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = fbd.SelectedPath;
+                LoadVideoFilesAsync(fbd.SelectedPath);
+            }
+        }
+    }
+}
